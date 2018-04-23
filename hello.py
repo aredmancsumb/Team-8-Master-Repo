999d06f2ec90fb55f268b7e7f8f2d483344cb8a5
Ricardo Baca <rbaca@csumb.edu>
2018-04-23 14:50:58 -0700

    Getting users input

---
+++ hello.py
@@ -0,0 +1,40 @@
+
+from flask import Flask, render_template, flash, request
+from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
+
+# App config.
+DEBUG = True
+app = Flask(__name__)
+app.config.from_object(__name__)
+app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
+
+class ReusableForm(Form):
+    name = TextField('Search:', validators=[validators.required()])
+
+
+@app.route("/", methods=['GET', 'POST'])
+def hello():
+    form = ReusableForm(request.form)   #for is the users search
+    #print(form)
+    #print (form.errors)
+    if request.method == 'POST':
+        name=request.form['name']
+        print (name)  #prints users search to the console
+    return render_template('hello.html', form=form)
+
+
+#@app.route('/page_info/<id>')
+#def page_info(id):
+#    for name in image_info: #checking the id to find the right key in the dictionary to get the right id, title, and flickr_user
+#        if name['id_1'] == id:
+#            id_1 = name['id_1']
+#            title = name['title']
+#            flickr_user = name['flickr_user']
+
+
+#    return render_template("page_info.html", picId_1 = id_1, title_1 = title, flickr_user_1 = flickr_user)
+
+
+
+if __name__ == "__main__":
+    app.run()
