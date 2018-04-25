091dfde007dc3d589300d668e21a2bb391c77d79
Ricardo Baca <rbaca@csumb.edu>
2018-04-25 15:45:17 -0700

    improved search

--- search.py
+++ search.py
@@ -4,19 +4,24 @@ from pprint import pprint
 
 swapi_urls = ['https://swapi.co/api/films/', 'https://swapi.co/api/people/', 'https://swapi.co/api/planets/', 'https://swapi.co/api/species/', 'https://swapi.co/api/starships/', 'https://swapi.co/api/vehicles/']
 
-user_search = input('Search:')
+def searchApi(user_search):
+    count = 0
+    for url in swapi_urls:
 
-data = []
+        params = {
+        'search': user_search
+        }
 
-for url in swapi_urls:
+        r = requests.get(url, params=params)
+        data = []
+        data.append(r.json())
+        \
+        for api in data:
+            if api['count'] >= 1:
+                pprint(api)
+                count += 1
+                
+    if count == 0:
+        pprint("try Again")
 
-    params = {
-    'search': user_search
-    }
-
-    r = requests.get(url, params=params)
-    data.append(r.json())
-
-for api in data:
-    if api['count'] >= 1:
-        pprint(api)
+                #api in dictionary -> get info
